c37b14f6554ff982e244455653b18c65e00656ce
Chephos <efosacharlesabu@gmail.com>
2023-09-11 22:31:17 +0100

    chore: production setup changes

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    # "django.contrib.staticfiles",
     "rest_framework",
     "api",
     "drf_yasg",
@@ -122,6 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
 
 STATIC_URL = "static/"
 
